0b95d33bc3b12c7884af302a9cecf16977e1d454
aimbot1526 <anshjaamwal2002@gmail.com>
2022-10-28 02:07:02 +0530

    more on dto

--- backend/dtos/movies.dto.js
+++ backend/dtos/movies.dto.js
@@ -1,22 +1,35 @@
 function getMovies(movies) {
 
     let actors = [];
+    let producer = {};
 
-    movies.actors.forEach((actor) => {
-        const newActor = {
-            name: actor.name,
-            gender: actor.gender,
-            dob: actor.dob,
-            bio: actor.bio
-        };
-        actors.push(newActor);
-    });
+    if (movies["producer"]) {
+        producer = {
+            name: movies.producer.name,
+            gender: movies.producer.gender,
+            dob: movies.producer.dob,
+            bio: movies.producer.bio
+        }
+    }
+
+    if (movies["actors"]) {
+        movies.actors.forEach((actor) => {
+            const newActor = {
+                name: actor.name,
+                gender: actor.gender,
+                dob: actor.dob,
+                bio: actor.bio
+            };
+            actors.push(newActor);
+        });
+    }
 
     return {
         name: movies.name,
         yearOfRelease: movies.yearOfRelease,
         plot: movies.plot,
         poster: movies.poster,
+        producer: producer,
         actors: actors
     }
 }
--- backend/routes/movie.routes.js
+++ backend/routes/movie.routes.js
@@ -10,28 +10,71 @@ router.get("/welcome", auth, (req, res) => {
     res.status(200).json({ hello: "Welcome" });
 });
 
-router.post('/add', async (req, res) => {
+router.post('/addmovie', async (req, res) => {
 
-    try {
-        const { name, yearOfRelease, plot, poster } = req.body;
+    const movieName = req.body.movie.name;
 
-        const existingMovie = await Movies.findOne({ name });
+    const existingMovie = await Movies.find({ name: movieName });
 
-        if (existingMovie) {
-            return res.status(409).send('Movie already exists');
-        } else {
-            const movie = await Movies.create({
-                name: name,
-                yearOfRelease: yearOfRelease,
-                plot: plot,
-                poster: poster
-            });
-
-            res.json(movie);
-        }
-    } catch (e) {
-        console.log(e);
+    if (existingMovie.length > 0) {
+        return res.status(409).send('Movie already exists');
     }
+
+    const { newMovieName, yearOfRelease, plot, poster } = req.body.movie;
+
+    var movie = await Movies.create({
+        name: newMovieName,
+        yearOfRelease: yearOfRelease,
+        plot: plot,
+        poster: poster
+    });
+
+    const { name, gender, dob, bio } = req.body.producer;
+
+    const producer = await Producer.create({
+        name: name,
+        gender: gender,
+        dob: dob,
+        bio: bio
+    });
+
+    await Movies.updateOne(
+        { _id: movie._id },
+        { $push: { producer: producer._id } },
+    );
+
+    req.body.actors.forEach(async function (actor) {
+        const newActor = await Actor.create({
+            name: actor.name,
+            gender: actor.gender,
+            dob: actor.dob,
+            bio: actor.bio
+        });
+        await Movies.updateOne(
+            { _id: movie._id },
+            { $push: { actors: newActor._id } },
+        );
+    })
+    return res.send("Movie Added Successfully");
+
+});
+
+router.get('/getmoveies', async (req, res) => {
+
+    const movieName = req.query.movie;
+
+    const exisitingMovie = await Movies.findOne({ name: movieName }).populate('actors').populate('producer');
+
+    console.log(exisitingMovie);
+
+    if(exisitingMovie.length === 0) {
+        return res.status(409).send(`No Movie found with name : ${movieName}`);
+    }
+
+    const dto = movieDto.getMovies(exisitingMovie);
+
+    res.json(dto);
+
 });
 
 router.post('/add/actor', async (req, res) => {
@@ -56,54 +99,6 @@ router.post('/add/actor', async (req, res) => {
     } catch (e) {
         console.log(e);
     }
-});
-
-router.post('/addActors', async (req, res) => {
-
-    const movieName = 'Sholay';
-
-    var exisitingMovie = await Movies.findOne({ movieName });
-
-    const { name, gender, dob, bio } = req.body.producer;
-
-    const producer = await Producer.create({
-        name: name,
-        gender: gender,
-        dob: dob,
-        bio: bio
-    });
-
-    await Movies.updateOne(
-        { _id: exisitingMovie._id },
-        { $push: { producer: producer._id } },
-    );
-    
-    req.body.actors.forEach(async function (actor) {
-        const newActor = await Actor.create({
-            name: actor.name,
-            gender: actor.gender,
-            dob: actor.dob,
-            bio: actor.bio
-        });
-        await Movies.updateOne(
-            { _id: exisitingMovie._id },
-            { $push: { actors: newActor._id } },
-        );
-    })
-    return res.json(exisitingMovie);
-
-});
-
-router.get('/getmoveies', async (req, res) => {
-
-    const name = 'Sholay';
-
-    const exisitingMovie = await Movies.findOne({ name }).populate('actors');
-
-    const dto = movieDto.getMovies(exisitingMovie);
-
-    res.json(dto);
-
 });
 
 module.exports = router;
